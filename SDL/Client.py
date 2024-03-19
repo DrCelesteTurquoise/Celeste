7a461a150593dead372e036daee8a32a5136cb4c
DrCelesteTurquoise <163599784+DrCelesteTurquoise@users.noreply.github.com>
2024-03-19 19:17:15 +0000

    add sdl

---
+++ SDL/Client.py
@@ -0,0 +1,13 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")  # 连接到服务器地址和端口
+
+# 发送消息给服务器
+message = "Hello, Server!"
+socket.send_string(message)
+
+# 等待服务器回复
+reply = socket.recv_string()
+print("从服务器收到回复: ", reply)
---
+++ SDL/Server.py
@@ -0,0 +1,15 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")  # 绑定到端口 5555
+
+print("等待客户端连接...")
+
+while True:
+    # 等待客户端请求
+    message = socket.recv_string()
+    print("收到请求: ", message)
+
+    # 回复消息给客户端
+    socket.send_string("服务端收到消息: {}".format(message))
