2bb43672e57a461d1057d723910681e4ee84d145
DrCelesteTurquoise <163599784+DrCelesteTurquoise@users.noreply.github.com>
2024-03-20 15:19:16 +0000

    add func

--- vino/SRequester.py
+++ vino/SRequester.py
@@ -2,14 +2,16 @@
 
 import zmq
 
-context = zmq.Context()
-requester = context.socket(zmq.REQ)
-requester.connect("tcp://localhost:5555")
+def create():
+    context = zmq.Context()
+    requester = context.socket(zmq.REQ)
+    requester.connect("tcp://localhost:5555")
 
-while True:
-    message = input("Enter your request: ")
-    requester.send_string(message)
+    while True:
+        message = input("Enter your request: ")
+        requester.send_string(message)
 
-    # 等待应答
-    response = requester.recv_string()
-    print(f"Received response: {response}")
+        # 等待应答
+        response = requester.recv_string()
+        print(f"Received response: {response}")
+
