179601494beafe05f3bb8d4031c24c4fd9c63b03
DrCelesteTurquoise <drcelesteturquoise@gmail.com>
2024-03-29 16:50:56 -0500

    big update re folders fast is top

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='vino',
-    version='0.0.6',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'pyzmq',
     ],
     url='https://github.com/DrCelesteTurquoise/Celeste.git',
     author='vino',
-    author_email='your_email@example.com',
+    author_email='drcelesteturquoise@gmail.com',
     description='VIONDDDD',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
