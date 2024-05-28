4d93606659ccdd803950572ddd828a7adb34f7ca
DrCelesteTurquoise <drcelesteturquoise@gmail.com>
2024-05-28 16:57:10 -0500

    gui for 250 and ur5e added

---
+++ vino/Mir/Entity_MiR250GUI.py
@@ -0,0 +1,120 @@
+# SDL in ANL Entity MiR250 Version 1.0 by TDai
+
+import sys
+import time
+import zmq
+import os
+
+import requests
+import json
+
+ip = '192.168.12.20'
+host = 'http://' + ip + '/api/v2.0.0/'
+
+headers = {}
+headers['Content-Type'] = 'application/json'
+headers['Authorization'] = 'Basic RGlzdHJpYnV0b3I6NjJmMmYwZjFlZmYxMGQzMTUyYzk1ZjZmMDU5NjU3NmU0ODJiYjhlNDQ4MDY0MzNmNGNmOTI5NzkyODM0YjAxNA=='
+
+        
+def mission_status_template(host):
+    url = 'status'
+    get_request = requests.get(host+url)
+    status_code = get_request.status_code
+    if status_code == 200:
+        txt = get_request.text
+        status_dict = json.loads(txt)
+        
+        battery_percentage = float(status_dict.get('battery_percentage'))
+        state = str(status_dict.get('state_text'))
+        
+        print('Battery Percentage:', int(battery_percentage))
+        print('Status:', state)
+    else:
+        print('MiRError:', status_code)
+        
+def mission_complete_check(host):
+    
+    while True:
+        # check every 3 sec 
+        time.sleep(3)
+        url = 'status'
+        get_request = requests.get(host+url)
+        txt = get_request.text
+        status_dict = json.loads(txt)        
+        state = str(status_dict.get('state_text'))
+        
+        if state == 'Ready':
+            break       
+        else:
+            continue
+
+
+def start_entity_MiR(Entity_name, sub_addr, sub_port, pub_addr, pub_port):
+    print(f"{Entity_name} Start")
+
+    context = zmq.Context()
+    sub = context.socket(zmq.SUB)
+    pub = context.socket(zmq.PUB)
+
+    try:
+        sub.connect(f"tcp://{sub_addr}:{sub_port}")
+        sub.setsockopt_string(zmq.SUBSCRIBE, Entity_name)
+        pub.bind(f"tcp://{pub_addr}:{pub_port}")
+        print(f"{Entity_name} Connected to the Host")
+    except zmq.ZMQError as e:
+        sub.close()
+        pub.close()
+        context.term()
+        print(f"{Entity_name} Connection Fail: {e}")
+        return
+
+    while True:
+        topic, cmd = sub.recv_multipart()
+        print(topic.decode(), cmd.decode())
+        cmd = cmd.decode()
+        if cmd == 'GPC':
+                        
+            mission_id = {"mission_id": "b0a59fbe-e87e-11ee-a42c-00012978ede1"} #TDaiGPC
+            requests.post(host + 'mission_queue', json = mission_id, headers = headers)
+            
+            # mission complete check every three secs
+            mission_complete_check(host)           
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name}: To GPC Completed\n')
+            print(f'{Entity_name} To GPC Completed\n')
+        elif cmd == 'Charger':
+            
+            mission_id = {"mission_id": "099a22b7-e878-11ee-a42c-00012978ede1"} #TDaiCharger
+            requests.post(host + 'mission_queue', json = mission_id, headers = headers)
+            
+            # mission complete check every three secs
+            mission_complete_check(host)           
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name}: To Charger Completed\n')
+            print(f'{Entity_name} To Charger Completed\n')
+        elif cmd == 'ChemSpeed':
+            
+            mission_id = {"mission_id": "2deb6dd8-0e43-11ef-9d95-00012978ede1"} #TDaiGPC
+            requests.post(host + 'mission_queue', json = mission_id, headers = headers)
+            
+            # mission complete check every three secs
+            mission_complete_check(host)           
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name}: To ChemSpeed Completed\n')
+            print(f'{Entity_name} To ChemSpeed Completed\n')
+        else:
+            print('Error! Plz rerun this file. Exit in 3 secs')
+            pub.send_string(f'{Entity_name} Error in Host Command\n')
+            time.sleep(3)
+            sys.exit(1)
+
+if __name__ == "__main__":
+    Entity_name = "MiR250"
+    sub_addr = "192.168.12.247"
+    sub_port = "56666"
+    pub_addr = "192.168.12.247"
+    pub_port = "56616"
+    start_entity_MiR(Entity_name, sub_addr, sub_port, pub_addr, pub_port)
---
+++ vino/UrE/Entity_UR5eGUI.py
@@ -0,0 +1,138 @@
+# SDL in ANL Entity UR5e Version 1.0 by TDai
+
+import sys
+import time
+import zmq
+import os
+import socket
+import logging
+
+# UR5eIP default ip = '192.168.12.249'
+
+class UR5eRemote:
+    def __init__(self, robotIP):
+        self.robotIP = robotIP
+        self.port = 29999
+        self.timeout = 5
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.getLogger().setLevel(logging.INFO)
+
+    def connect(self):
+        self.sock.settimeout(self.timeout)
+        self.sock.connect((self.robotIP, self.port))
+        self.sock.recv(1096)
+
+    def sendAndReceive(self, command):
+        try:
+            self.sock.sendall((command + '\n').encode())
+            return self.get_reply()
+        except (ConnectionResetError, ConnectionAbortedError):
+            logging.warning('The connection was lost to the robot. Please connect and try running again.')
+            self.close()
+            sys.exit()
+
+    def get_reply(self):
+        """
+        read one line from the socket
+        :return: text until new line
+        """
+        collected = b''
+        while True:
+            part = self.sock.recv(1)
+            if part != b"\n":
+                collected += part
+            elif part == b"\n":
+                break
+        return collected.decode("utf-8")
+    
+    def program_complete_check(self):
+        while True:
+            time.sleep(3)
+            state = self.sendAndReceive('programState')
+            
+            if 'STOPPED' in state:
+                break       
+            else:
+                continue
+
+    def close(self):
+        self.sock.close()
+
+
+
+def start_entity_UR5e(Entity_name, sub_addr, sub_port, pub_addr, pub_port, ur5e_ip):
+    print(f"{Entity_name} Start")
+
+    context = zmq.Context()
+    sub = context.socket(zmq.SUB)
+    pub = context.socket(zmq.PUB)
+    
+    ur5eremote = UR5eRemote(ur5e_ip)
+
+    try:
+        sub.connect(f"tcp://{sub_addr}:{sub_port}")
+        sub.setsockopt_string(zmq.SUBSCRIBE, Entity_name)
+        pub.bind(f"tcp://{pub_addr}:{pub_port}")
+        print(f"{Entity_name} Connected to the Host")
+        
+        ur5eremote.connect()
+        remoteCheck = ur5eremote.sendAndReceive('is in remote control')
+        if 'false' in remoteCheck:
+            logging.warning('Robot is in local mode. Some commands may not function.')
+        
+    except zmq.ZMQError as e:
+        sub.close()
+        pub.close()
+        context.term()
+        
+        ur5eremote.close()
+        
+        print(f"{Entity_name} Connection Fail: {e}")
+        return
+
+    while True:
+        topic, cmd = sub.recv_multipart()
+        print(topic.decode(), cmd.decode())
+        cmd = cmd.decode()
+        if cmd == 'TDai':
+            
+            ur5eremote.sendAndReceive('load TDai.urp')
+            time.sleep(2)
+            ur5eremote.sendAndReceive('play')
+            ur5eremote.program_complete_check()
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name} Action TDai Completed\n')
+            print(f'{Entity_name} Action TDai Completed\n')
+        elif cmd == 'ToolChangeDemo':
+            ur5eremote.sendAndReceive('load TDaiToolChangeDemo.urp')
+            time.sleep(2)
+            ur5eremote.sendAndReceive('play')
+            ur5eremote.program_complete_check()
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name} Action ToolChangeDemo Completed\n')
+            print(f'{Entity_name} Action ToolChangeDemo Completed\n')
+        elif cmd == 'LoadVial2ChemS':
+            ur5eremote.sendAndReceive('load TDaiLoadVial2ChemS.urp')
+            time.sleep(2)
+            ur5eremote.sendAndReceive('play')
+            ur5eremote.program_complete_check()
+            time.sleep(1)
+            
+            pub.send_string(f'{Entity_name} Action TDaiLoadVial2ChemS Completed\n')
+            print(f'{Entity_name} Action TDaiLoadVial2ChemS Completed\n')
+        else:
+            print('Error! Plz rerun this file. Exit in 3 secs')
+            pub.send_string(f'{Entity_name} Error in Host Command\n')
+            time.sleep(3)
+            sys.exit(1)
+
+if __name__ == "__main__":
+    Entity_name = "UR5e"
+    sub_addr = "192.168.12.247" # host
+    sub_port = "56666" # host
+    pub_addr = "192.168.12.247"
+    pub_port = "56626"
+    ur5e_ip = '192.168.12.249'
+    start_entity_UR5e(Entity_name, sub_addr, sub_port, pub_addr, pub_port, ur5e_ip)
